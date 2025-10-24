da89d5c97e73a045c092d6a3f61bb858dd2d6434
driftmeisterbj <ak.stra88@gmail.com>
2025-10-24 22:09:02 +0200

    Overtført profile, index, assets og components Button og PostCard. Gjenkjenner ikke react-native, prøver restart

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,100 @@
+import { ProfileSettingCard } from "../../components/SettingCard";
+
+import { Image } from "expo-image";
+import { ScrollView, Text, View } from "react-native";
+
+import image from "../../assets/beluga.png";
+import PostCard from "../../components/PostCard";
+import { styles } from "../components/styles";
+
+import Button from "../../components/Button";
+
+interface ProfileProps {
+  imageUrl?: string;
+  username?: string;
+  verified?: boolean;
+  phoneNumber?: string;
+  email?: string;
+  posts?: Post[];
+}
+
+interface Post {
+  title: string;
+  imageUrl?: string;
+  id: string;
+}
+
+const postsDummy = [
+  { title: "Post 1", id: "1" },
+  { title: "Post 2", id: "2" },
+  { title: "Post 3", id: "3" },
+  { title: "Post 4", id: "4" },
+  { title: "Post 5", id: "5" },
+  { title: "Post 6", id: "6" },
+  { title: "Post 7", id: "7" },
+  { title: "Post 8", id: "8" },
+  { title: "Post 9", id: "9" },
+  { title: "Post 10", id: "10" },
+];
+
+const dummyProfileData = {
+  imageUrl: image,
+  username: "Katt",
+  verified: true,
+  phoneNumber: "12345678",
+  email: "katt@example.com",
+};
+
+const Profile = ({
+  imageUrl = dummyProfileData.imageUrl,
+  username = dummyProfileData.username,
+  verified = dummyProfileData.verified,
+  phoneNumber = dummyProfileData.phoneNumber,
+  email = dummyProfileData.email,
+  posts = postsDummy,
+}: ProfileProps) => {
+  return (
+    <ScrollView>
+      <View style={styles.profileContainer}>
+        <View style={styles.profileHeaderContainer}>
+          <Image
+            source={imageUrl ? { uri: imageUrl } : image}
+            style={styles.profileImage}
+          />
+          <Text style={styles.profileHeaderText}>{username}</Text>
+          <Text style={[styles.profileHeaderText, styles.profileHeaderText]}>
+            {verified ? "Verifisert medlem" : "Uverifisert medlem"}
+          </Text>
+        </View>
+        <View style={styles.profileContainerMiddle}>
+          <ProfileSettingCard
+            setting="Telefonnummer"
+            settingInfo={phoneNumber}
+          />
+          <ProfileSettingCard setting="E-post" settingInfo={email} />
+          <Button
+            text="Lagre endringer"
+            path="../settings"
+            buttonStyle={styles.profileButton}
+            buttonTextStyle={styles.profileText}
+          />
+        </View>
+        <View style={styles.profileContainerMiddle}>
+          <Text>Mine innlegg</Text>
+          <View style={styles.profilePostsView}>
+            {posts.map((post) => (
+              <PostCard
+                key={post.id}
+                title={post.title}
+                imageUrl={post.imageUrl}
+                id={post.id}
+              />
+            ))}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Profile;
---
+++ app/styles.ts
@@ -0,0 +1,67 @@
+import { StyleSheet } from "react-native";
+
+const allStyles = StyleSheet.create({
+  ProfileButton: {
+    backgroundColor: "transparent",
+    padding: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#e0e0e0e8",
+  },
+  ProfileText: {
+    fontSize: 16,
+  },
+  ProfileHeaderText: {
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 8,
+  },
+  ProfileMiddleContainer: {
+    alignItems: "center",
+    gap: 12,
+    width: "100%",
+  },
+  ProfileContainer: {
+    flex: 1,
+    paddingBottom: 30,
+    paddingTop: 20,
+    gap: 45,
+    width: "90%",
+    alignItems: "center",
+    alignSelf: "center",
+  },
+  ProfileHeaderContainer: {
+    alignItems: "center",
+  },
+  ProfileSettingCardContainer: {
+    flexDirection: "row",
+    width: "100%",
+  },
+  ProfileImage: {
+    width: 90,
+    height: 90,
+    borderWidth: 4,
+    borderRadius: 90,
+    borderColor: "#E1F8D7",
+  },
+  ProfilePostsView: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    gap: 12,
+    justifyContent: "space-between",
+  },
+});
+
+export const styles = StyleSheet.create({
+  profileButton: allStyles.ProfileButton,
+  profileText: allStyles.ProfileText,
+  profileContainer: allStyles.ProfileContainer,
+  profileHeaderContainer: allStyles.ProfileHeaderContainer,
+  profileSettingCardContainer: allStyles.ProfileSettingCardContainer,
+  profileHeaderText: allStyles.ProfileHeaderText,
+  profileContainerMiddle: allStyles.ProfileMiddleContainer,
+  profileImage: allStyles.ProfileImage,
+  profilePostsView: allStyles.ProfilePostsView,
+});
