2b7fae58644ad8d89e4096cc1c1402f2bf11a09c
chanipa <Chanipa.dencharoen@gmail.com>
2025-10-29 12:22:00 +0100

    blank side

---
+++ app/(tabs)/newPlaces.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const NewPlaces = () => {
+    return (
+        <View>
+            <Text>Nytt sted</Text>
+        </View>
+    )
+}
+export default NewPlaces;
