dcfebf3341f89cd6cb902766bffd1a19bdf9c071
Emil-18 <emilhesmyr@icloud.com>
2025-11-01 19:28:27 +0100

    Added the <Camera> component, to see if the react-native-camera library workes as it should

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Pressable, TextInput, View } from "react-native";
 import Button from "@/components/Button";
 import SearchableList from "@/components/searchableList";
-
+import Camera from "react-native-camera";
 export default class Home extends React.Component {
     placeHolderPearls = [{ id: "1", title: "pearl1" }, {id: "2", title: "pearl2"}]
 
@@ -13,10 +13,10 @@ export default class Home extends React.Component {
         var output = <View>
             <Button text = "Min profil" path = "/profile"></Button>
             <Button text = "Innstillinger" path = "/usersettings"></Button>
-            <SearchableList data = {this.placeHolderPearls} placeHolderText = "sok etter perle steder"></SearchableList>
+            <SearchableList data={this.placeHolderPearls} placeHolderText="sok etter perle steder"></SearchableList>
+            <Camera></Camera>
 
         </View>
-        this.output = output;
         return (output);
     }
 
