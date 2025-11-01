06107ab64d4b4ff27e60ae5a6e913d7b10679900
Emil-18 <emilhesmyr@icloud.com>
2025-11-01 18:25:57 +0100

    Added navigation buttons to settings and profile

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,13 +4,15 @@ import Button from "@/components/Button";
 import SearchableList from "@/components/searchableList";
 
 export default class Home extends React.Component {
-    placeHolderPearls = [{ id: "1", title: "pearl1", }, {id: "2", title: "pearl2"}]
+    placeHolderPearls = [{ id: "1", title: "pearl1" }, {id: "2", title: "pearl2"}]
 
 
     
 
     render() { 
         var output = <View>
+            <Button text = "Min profil" path = "/profile"></Button>
+            <Button text = "Innstillinger" path = "/usersettings"></Button>
             <SearchableList data = {this.placeHolderPearls} placeHolderText = "sok etter perle steder"></SearchableList>
 
         </View>
