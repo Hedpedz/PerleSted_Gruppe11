370d3f1d364c912613549e1e52d3c0130f8a9e34
driftmeisterbj <ak.stra88@gmail.com>
2025-11-04 20:14:43 +0100

    Update feed.tsx

--- app/(tabs)/feed.tsx
+++ app/(tabs)/feed.tsx
@@ -4,45 +4,46 @@ import { FlatList, StyleSheet, View } from "react-native";
 import { PearlCard } from "../../components/pearl/PearlCard";
 import { DummyPearls } from "../../data/pearlsDummy";
 
-const Feed = () => { 
-    return ( 
-        <FlatList data={DummyPearls} 
-        keyExtractor={(item) => item.id} //each item has i own Id 
-        renderItem={({item}) => (
-            <PearlCard
-            id={item.id}
-            title={item.title}
-            imageLocal={item.imageLocal}
-            //imageUrl={item.imageUrl}
-            
-            onPress={() => router.push({ 
-                pathname: "pearl/[id]",
-                params: { id: String(item.id) } })}
+const Feed = () => {
+  return (
+    <FlatList
+      data={DummyPearls}
+      keyExtractor={(item) => item.id} //each item has i own Id
+      renderItem={({ item }) => (
+        <PearlCard
+          id={item.id}
+          title={item.title}
+          imageLocal={item.imageLocal}
+          //imageUrl={item.imageUrl}
 
-            />
-            )
-        } 
-        ListHeaderComponent={ 
-            <View style={styles.content}> 
-            <View style={styles.row}> 
-            </View> 
-        </View> 
-        } /> 
-    )} 
-    const styles = StyleSheet.create({ 
-
-        container: { 
-            flex: 1,
-            backgroundColor: "#fcfffd",
-        }, 
-        content: {
-            paddingHorizontal: 16,
-            paddingTop: 12,
-        }, 
-        row: {
-            flexDirection: "row",
-            alignItems: "center", // ถ้าจะมีหลายการ์ดค่อยใช้ FlatList + numColumns/columnWrapperStyle 
-    },
-    
-    }); 
-    export default Feed;
+          onPress={() =>
+            router.push({
+              pathname: "Pearl/[id]",
+              params: { id: String(item.id) },
+            })
+          }
+        />
+      )}
+      ListHeaderComponent={
+        <View style={styles.content}>
+          <View style={styles.row}></View>
+        </View>
+      }
+    />
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fcfffd",
+  },
+  content: {
+    paddingHorizontal: 16,
+    paddingTop: 12,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center", // ถ้าจะมีหลายการ์ดค่อยใช้ FlatList + numColumns/columnWrapperStyle
+  },
+});
+export default Feed;
